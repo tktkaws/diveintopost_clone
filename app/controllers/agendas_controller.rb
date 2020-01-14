3c9488e98abe6a78dd612755614f917bcc5c037e
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-01-14 10:14:31 +0900

    Add: destroyアクション/削除権限

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,5 @@
 class AgendasController < ApplicationController
-  # before_action :set_agenda, only: %i[show edit update destroy]
+   before_action :set_agenda, only: %i[destroy]
 
   def index
     @agendas = Agenda.all
@@ -19,6 +19,17 @@ class AgendasController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    if current_user == @agenda.user || current_user == @agenda.team.owner
+      @agenda.destroy
+      redirect_to dashboard_url, notice: "成功"
+    end
+    # Agendaが削除されると、そのAgendaに紐づいているTeamに所属しているユーザー全員に通知メールが飛ぶ
+    # 情報処理が完了した後はDashBoardに飛ぶ
+    # redirect_to dashboard_url notice: 成功
+
   end
 
   private
